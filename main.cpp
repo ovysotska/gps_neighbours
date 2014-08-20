57b8e4bffe7d6f5cd077752e6775c84c9fe36318
Olga <olga.vysotska@uni-bonn.de>
2014-08-20 12:55:38 +0200

    writing the names to file now

--- main.cpp
+++ main.cpp
@@ -5,12 +5,34 @@
 #include <vector>
 #include <string>
 
-#define RANGE 1.0
+#define RANGE 0.2
 
 using namespace std;
 
+//TODO: pass paramerters from script as [argv, argc]
 const string DATA_DIR_W = "/home/olga/projects/sceneMatching_local/freiburgcar-seasons/log_12_12_2012_____12_56_59/";
 const string DATA_DIR_S = "/home/olga/projects/sceneMatching_local/freiburgcar-seasons/log_24_5_2012_____11_11_47/";
+const string neighbourFile = "../results/neigh_images.txt";
+
+void writeToFile(vector<string> &img_names, const string &file_name)
+{
+	ofstream out(file_name.c_str());
+	if (out)
+	{
+		for(vector<string>::iterator iter = img_names.begin();  iter!= img_names.end(); ++iter)
+		{
+			out << *iter << endl;
+		}
+		cout << "Selected neighbouring image names were written to " << file_name << endl; 
+	}
+	else
+	{
+		cout << "Unable to open a file "<< file_name << endl;
+		return;
+	}
+}
+
+
 int main()
 {
     cout << "\t Finding nearest images in database based on query image gps" << endl;
@@ -25,6 +47,7 @@ int main()
     summer_db.get_img_coord(sum_img_lat, sum_img_lon);
     winter_db.find_neigh(sum_img_lat, sum_img_lon, img_in_range, RANGE);
 
+    writeToFile(img_in_range, neighbourFile);
 
     cout << "\t Done." << endl;
 }
